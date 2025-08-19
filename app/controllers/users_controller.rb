9fcc2ce3605267adc829230ad44c1356203d9da6
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-08-19 13:48:59 +0900

    add show and set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def show
+    @conversations = @user.conversations.order(created_at: :desc)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+    redirect_to(root_path, alert: "Not authorized") unless @user == current_user
   end
 end
