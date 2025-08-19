8959b3c0dde3101daf35718fe4e2955d783d96dd
pedro sassaki <pedropq2001@gmail.com>
2025-08-19 14:02:06 +0900

    generated conversation controller with its corresponding views

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,25 @@
+class ConversationsController < ApplicationController
+  def index
+    @conversations = Conversations.All
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+# ----------------------------------------------------------------------------------#
+# TODO: make the user able to edit the conversation title (FOR LATER IF POSSIBLE)
+  # def edit
+  # end
+
+  # def update
+  # end
+# ----------------------------------------------------------------------------------#
+end
