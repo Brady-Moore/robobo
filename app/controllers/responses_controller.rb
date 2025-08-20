a711edda7b15e4bdb76843778f7835bb903ef139
Pedro Sassaki <pedropq2001@gmail.com>
2025-08-20 16:51:51 +0900

    "ai has been implemented" (#13)

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,10 +3,12 @@ class ResponsesController < ApplicationController
   before_action :set_conversation
 
   def create
-    @response = @conversation.responses.build(response_params)
+    @response = Response.new(response_params)
+    @response.conversation = @conversation
     @response.from_user = true
-
+    
     if @response.save
+      @ai = AiMessageService.new(@response).call
       redirect_to conversation_path(@conversation, anchor: "bottom")
     else
       @responses = @conversation.responses.order(:created_at)
@@ -14,7 +16,7 @@ class ResponsesController < ApplicationController
       render "conversations/show", status: :unprocessable_entity
     end
   end
-
+  
   private
 
   def set_conversation
