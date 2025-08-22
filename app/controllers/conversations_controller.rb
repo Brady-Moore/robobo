4b38a5f7f0dfbeca7cba5604d56511682fd12afa
Pedro Sassaki <pedropq2001@gmail.com>
2025-08-22 14:03:10 +0900

    Offcanvas (#26)
    * removed white spaces and fixed gemfile.lock
    
    * added ofcanvas

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -15,6 +15,7 @@ class ConversationsController < ApplicationController
   def new
     @conversation = Conversation.new
     @demo_conversation = Conversation.find_by(title: "Demo Conversation")
+    @conversations = current_user.conversations.order(created_at: :desc)
   end
 
   def create
